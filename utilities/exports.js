be15f092f322ae77d22d7fb585673b89842e2562
jaspersn <jasper.s.newkirk@gmail.com>
2023-06-04 18:52:48 -0700

    Added endpoints for password reset

--- utilities/exports.js
+++ utilities/exports.js
@@ -5,6 +5,7 @@ module.exports = {
     pool: require('./sql_conn.js'), // Connection to Heroku Database
     generateHash: credUtils.generateHash,
     generateSalt: credUtils.generateSalt,
+    generatePassword: credUtils.generatePassword,
     validation: require('./validationUtils.js'),
     sendEmail: require('./email.js').sendEmail,
     registerUtils: require('./registerUtils'),
